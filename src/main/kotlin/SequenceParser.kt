87ca22b0feee711e19adcba1c827766a0fdef657
Aldo Malerba <aldomalerba.dev@gmail.com>
2021-10-06 19:14:30 +0200

    create dummy parser

--- src/main/kotlin/SequenceParser.kt
+++ src/main/kotlin/SequenceParser.kt
@@ -1,7 +1,9 @@
 class SequenceParser {
 
     fun parse(number: Int) : String {
-        return ""
+        if(number < 15)
+            return "1, Wizz, FizzWizz, 4, BuzzWizz, Fizz, Wizz, 8, Fizz, Buzz, Wizz, Fizz, Wizz, 14, FizzBuzz".split(",")[number - 1]
+        else return ""
     }
 
 }
